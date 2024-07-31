2b5a9fe627bc45ee3d950dae5805028d1aeb7710
Sae <67304912+DracNami@users.noreply.github.com>
2024-07-31 01:12:48 -0400

    bandaid fix for weird collectable bug

--- pirate jam shadow/Assets/Scripts/Objectives/CollectObjective.cs
+++ pirate jam shadow/Assets/Scripts/Objectives/CollectObjective.cs
@@ -31,7 +31,7 @@ public class CollectObjective : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(collected == collectables)
+        if(collected >= collectables)
         {
             collectedAll = true;
         }
